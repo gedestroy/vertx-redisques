7925bf7a24d8379d2a5968153badb75fe011a7a2
Ball, Nware <ball@nware.in.th>
2022-08-12 10:20:48 +0700

    try to run release

---
+++ .github/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git config --global user.email "swisspush@post.ch"
+git config --global user.name "Github-CI"
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
