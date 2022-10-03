972f5f8a8f9e09a38a113c24b9a30cdc1c846139
Ball, Nware <ball@nware.in.th>
2022-10-03 16:23:30 +0700

    try to release

--- .github/maybe-release-github.sh
+++ .github/maybe-release-github.sh
@@ -14,8 +14,7 @@ then
     echo 'starting a new nexus repository ...'
     OUTPUT=$(groovy staging.groovy start)
     echo "repository Id: $OUTPUT"
-    export GPG_TTY=$(tty)
-    mvn -B -Prelease jgitflow:release-start jgitflow:release-finish --settings settings.xml -DrepositoryId=${OUTPUT}
+    mvn -B -Prelease jgitflow:release-start jgitflow:release-finish -DrepositoryId=${OUTPUT}
     rc=$?
     if [ $rc -eq 0 ]
     then
