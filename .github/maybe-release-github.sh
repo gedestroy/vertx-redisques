cad43cbeff505ca298bda5f08e4f772110392b0f
Ball, Nware <ball@nware.in.th>
2022-10-06 16:23:49 +0700

    do not double-check it

--- .github/maybe-release-github.sh
+++ .github/maybe-release-github.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 set -ev
-if [ "$GITHUB_REF_NAME" == "master" ] && [ "$GITHUB_REF_PROTECTED" == "false" ] && [ "$GITHUB_REPOSITORY" == "gedestroy/vertx-redisques" ]
+git fetch
+git reset --hard
+git clean -fd
+git pull
+groovy staging.groovy drop
+rc=$?
+if [ $rc -ne 0 ]
 then
-    git fetch
-    git reset --hard
-    git clean -fd
-    git pull
-    groovy staging.groovy drop
+  echo 'problem when trying to drop, ignored'
+fi
+echo 'starting a new nexus repository ...'
+OUTPUT=$(groovy staging.groovy start)
+echo "repository Id: $OUTPUT"
+mvn -B -Prelease jgitflow:release-start jgitflow:release-finish -DrepositoryId=${OUTPUT}
+rc=$?
+if [ $rc -eq 0 ]
+then
+    groovy staging.groovy close ${OUTPUT}
+    groovy staging.groovy promote ${OUTPUT}
     rc=$?
     if [ $rc -ne 0 ]
     then
-      echo 'problem when trying to drop, ignored'
+      echo 'Release failed, cannot promote stage'
+      exit $rc
     fi
-    echo 'starting a new nexus repository ...'
-    OUTPUT=$(groovy staging.groovy start)
-    echo "repository Id: $OUTPUT"
-    mvn -B -Prelease jgitflow:release-start jgitflow:release-finish -DrepositoryId=${OUTPUT}
-    rc=$?
-    if [ $rc -eq 0 ]
-    then
-        groovy staging.groovy close ${OUTPUT}
-        groovy staging.groovy promote ${OUTPUT}
-        rc=$?
-        if [ $rc -ne 0 ]
-        then
-          echo 'Release failed, cannot promote stage'
-          exit rc
-        fi
-        echo 'Release done, will push'
-        git tag
-        git push --tags
-        git checkout develop
-        git push origin develop
-      exit 0
-    fi
-    echo 'Release failed'
-    exit rc
-else
-    echo 'Release skipped'
-    exit 0
+    echo 'Release done, will push'
+    git tag
+    git push --tags
+    git checkout develop
+    git push origin develop
+  exit 0
 fi
+echo 'Release failed'
+exit $rc
